9486c4a6d371209b3262f3b09a473b9072fa1b28
your name <37942959+Sardor979bek@users.noreply.github.com>
2024-01-13 05:36:40 -0500

    add Api Gateway and configure

--- api-gateway/src/main/java/gateway/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/gateway/apigateway/ApiGatewayApplication.java
@@ -1,4 +1,4 @@
-package identityservice;
+package gateway.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,10 +6,10 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class IdentityServiceApplication {
+public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(IdentityServiceApplication.class, args);
+		SpringApplication.run(ApiGatewayApplication.class, args);
 	}
 
 }
---
+++ api-gateway/src/main/java/gateway/apigateway/filter/AuthenticationFilter.java
@@ -0,0 +1,51 @@
+package gateway.apigateway.filter;
+
+import gateway.apigateway.util.JwtUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AuthenticationFilter extends AbstractGatewayFilterFactory<AuthenticationFilter.Config> {
+    private final RouteValidator validator;
+    private final JwtUtil jwtUtil;
+
+    @Autowired
+    public AuthenticationFilter(RouteValidator validator, JwtUtil jwtUtil) {
+        super(Config.class);
+        this.validator = validator;
+        this.jwtUtil = jwtUtil;
+    }
+
+    @Override
+    public GatewayFilter apply(Config config) {
+        return (exchange, chain) -> {
+            ServerHttpRequest request = exchange.getRequest();
+
+            if (validator.isSecured.test(request)) {
+                String authHeader = request.getHeaders().getFirst("Authorization");
+
+                if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+                    exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+                    return exchange.getResponse().setComplete();
+                }
+
+                try {
+                    jwtUtil.validateToken(authHeader.substring(7));
+                } catch (Exception e) {
+                    // Log for debugging
+                    exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+                    return exchange.getResponse().setComplete();
+                }
+            }
+            return chain.filter(exchange);
+        };
+    }
+
+    public static class Config {
+        // Configuration properties here, if needed
+    }
+}
